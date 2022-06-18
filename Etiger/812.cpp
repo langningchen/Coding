76424ceeb16df051079724030fe31f5541e29e19
langningchen <81672884+langningchen@users.noreply.github.com>
2022-06-18 12:47:01 +0000

    vim 812.cpp

--- Etiger/812.cpp
+++ Etiger/812.cpp
@@ -1,7 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-void tra
+typedef long long ll;
+ll n, m;
 int main() {
-	
+	cin >> n >> m;
+	for (ll i = 1; i <= m; i++) {
+		ll u ,v;
+		cin >> u >> v;
+		to[u].push_back(v);
+		to[v].push_back(u);
+	}
+	tarjan(rt = 1, 0);
+	ll nCnt = 0;
+	for (ll u = 1; u <= n; ++u) {
+		if (isCut(u)) {
+			nCnt++;
+			cout << u << endl;
+		}
+	}
+	if (!nCnt) {
+		cout << "perfect" << endl;
+	}
 	return 0;
 }
