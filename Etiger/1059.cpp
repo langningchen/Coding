32f8af5265ef12d978c20a638b2976a76fec451b
langningchen <81672884+langningchen@users.noreply.github.com>
2022-07-07 07:31:52 +0000

    Finish 1059

--- Etiger/1059.cpp
+++ Etiger/1059.cpp
@@ -2,33 +2,33 @@
 using namespace std;
 typedef long long ll;
 const int N = 100005;
-const int INF = 0x7FFFFFFF;
-ll m, n, B, a[N], as[N], b[N], x[N], y[N];
-ll quert(ll l, ll r, ll k) {
-	ll cnt = 0;
-	ll Max = -INF;
+const ll INF = 0x7FFFFFFFFFFFFFFF;
+ll m, n, B, b[N], x[N], y[N];
+pair<ll, ll> a[N], as[N];
+ll query(ll l, ll r, ll k) {
+	pair<ll, ll> cnt;
 	if (b[l] == b[r]) {
-		for (int i = l; i <= r; i++) if (a[i] <= k && a[i] > Max) cnt = i;
-		return cnt;
+		for (int i = l; i <= r; i++) if (a[i] <= make_pair(k, INF)) cnt = max(cnt, a[i]);
+		return cnt.second;
 	}
-	for (int i = l; b[i] == b[l]; i++) if (a[i] <= k && a[i] > Max) cnt = i;
-	for (int i = r; b[i] == b[r]; i--) if (a[i] <= k && a[i] > Max) cnt = i;
+	for (int i = l; b[i] == b[l]; i++) if (a[i] <= make_pair(k, INF)) cnt = max(cnt, a[i]);
+	for (int i = r; b[i] == b[r]; i--) if (a[i] <= make_pair(k, INF)) cnt = max(cnt, a[i]);
 	for (int j = b[l] + 1; j <= b[r] - 1; j++) {
-		ll *tmp = (upper_bound(as + x[j], as + y[j] + 1, k) - 1);
-		if (*tmp > Max) cnt = tmp - 
+		pair<ll, ll> tmp = *(upper_bound(as + x[j], as + y[j] + 1, make_pair(k, INF)) - 1); 
+		if (tmp <= make_pair(k, INF)) cnt = max(cnt, tmp);
 	}
-	return cnt;
+	return cnt.second;
 }
 int main() {
-//	freopen("marathon.in", "r", stdin);
-//	freopen("marathon.out", "w", stdout);
+	freopen("marathon.in", "r", stdin);
+	freopen("marathon.out", "w", stdout);
 	cin >> n >> m;
 	for (ll i = 1; i <= n; i++) {
 		int a1, b1, c1;
 		char c2;
 		cin >> a1 >> c2 >> b1 >> c2 >> c1 >> c2;
-		a[i] = 60 * 60 * a1 + 60 * b1 + c1;
-		cout << a[i] << endl;
+		a[i].second = i;
+		a[i].first = 60 * 60 * a1 + 60 * b1 + c1;
 	}
 	B = sqrt(n * log2(n));
 	for (ll i = 1; i <= n; i++) b[i] = (i - 1) / B + 1;
@@ -43,7 +43,7 @@ int main() {
 	for (ll i = 1; i <= m; i++) {
 		ll l, r, k;
 		cin >> l >> r >> k;
-		cout << quert(l, r, k) << " ";
+		cout << query(l, r, k) << " ";
 	}
 	return 0;
 } 
