0f1f830beb925e712725b71955d1835d8d4eb9bd
langningchen <81672884+langningchen@users.noreply.github.com>
2022-07-08 00:57:55 +0000

    start1010

---
+++ Etiger/1010.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 200005;
+int main() {
+    // freopen("premiere3.in", "r", stdin);
+    // freopen("premiere3.out", "w", stdout);
+    return 0;
+}
