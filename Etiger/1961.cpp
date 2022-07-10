f12fd3f155bebf4e9166267dc954cb73557aac8f
langningchen <81672884+langningchen@users.noreply.github.com>
2022-07-10 09:59:27 +0000

    unfinish 1961

---
+++ Etiger/1961.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll N = 200005;
+ll n, q, L;
+ll d[N], p[N][105], t[N];
+ll timer, tI[N], tO[N];
+struct edge {
+    ll to, nxt;
+} e[N * 2];
+ll nE, hd[N];
+void addE(ll u, ll v) {
+    e[++nE].to = v;
+    e[nE].nxt = hd[u];
+    hd[u] = nE;
+    e[++nE].to = u;
+    e[nE].nxt = hd[v];
+    hd[v] = nE;
+}
+void dfs(ll u, ll fa) {
+    tI[u] = ++timer;
+    d[u] = d[fa] + 1;
+    p[u][0] = fa;
+    for (ll i = 1; i <= L; i++) {
+        p[u][i] = p[p[u][i - 1]][i - 1];
+    }
+    for (ll i = hd[u]; i; i = e[i].nxt) {
+        ll v = e[i].to;
+        if (v == fa) continue;
+        dfs(v, u);
+    }
+    tO[u] = ++timer;
+}
+bool up(ll u, ll v) {
+    return !u || tI[u] <= tI[v] && tO[v] <= tO[u];
+}
+ll lca(ll u, ll v) {
+    if (up(u, v)) return u;
+    if (up(v, u)) return v;
+    for (ll i = L; i >= 0; i--) {
+        if (!up(p[u][i], v)) {
+            u = p[u][i];
+        }
+    }
+    return p[u][0];
+}
+ll dst(ll u, ll v) {
+    return d[u] + d[v] - 2 * d[lca(u, v)];
+}
+int main() {
+    freopen("visit.in", "r", stdin);
+    freopen("visit.out", "w", stdout);
+    cin >> n;
+    for (ll i = 1; i <= n - 1; i++) {
+        ll u, v;
+        cin >> u >> v;
+        addE(u, v);
+    }
+    L = log(n) / log(2) + 1;
+    dfs(1, 0);
+    memcpy(t, d, sizeof(d));
+    ll B = max_element(d + 1, d + 1 + n) - d;
+    dfs(B, 0);
+    ll C = max_element(d + 1, d + 1 + n) - d;
+    memcpy(d, t, sizeof(t));
+    ll ans = 0;
+    for (ll A = 1; A <= n; A++) {
+        ll AB = dst(A, B);
+        ll AC = dst(A, C);
+        ll cost = min(AB, AC) + dst(B, C);
+        ans = max(ans, cost);
+    }
+    cout << ans << endl;
+    return 0;
+}
