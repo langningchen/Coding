00ab27d77ee6eaa979141dd8c52c9ce5bf51dc8c
langningchen <81672884+langningchen@users.noreply.github.com>
2022-07-27 01:43:13 +0000

    finish do auto compile works

--- DoAutoCompileWorks.cpp
+++ DoAutoCompileWorks.cpp
@@ -1,7 +1,13 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <set>
+#include <thread>
+#include <vector>
 #include <dirent.h>
+#include <unistd.h>
+#include <sys/stat.h>
 using namespace std;
-const int ThreadCount = 10;
+const int ThreadCount = 16;
 set<string> ThreadCompileList[ThreadCount];
 thread ThreadList[ThreadCount];
 set<string> CompileList;
@@ -10,6 +16,11 @@ set<string> CompileSuccessList;
 set<string> CompileFailList;
 vector<string> SumaryList;
 int ThreadFinish;
+bool IsFileExists(string FileName)
+{
+    struct stat Temp;
+    return stat(FileName.c_str(), &Temp) == 0;
+}
 void StringReplaceAll(string &Data, string Before, string After)
 {
     int FindPos = 0;
@@ -21,15 +32,20 @@ void Compile(int ID)
     while (!ThreadCompileList[ID].empty())
     {
         string SourceFileName = *ThreadCompileList[ID].begin();
-        ThreadCompileList[ID].erase(0);
+        ThreadCompileList[ID].erase(ThreadCompileList[ID].begin());
         string DestFileName = SourceFileName.substr(0, SourceFileName.size() - 4);
         string CompileOutput = "";
-        FILE *PipePointer = popen(string("gcc " + SourceFileName + " -o " + DestFileName + " -O2 -lstdc++ -lm 2>&1").c_str(), "r");
+        FILE *PipePointer = popen(string("gcc " + SourceFileName + " -o " + DestFileName + " -O2 -lstdc++ -lm -lpthread -lcurl 2>&1").c_str(), "r");
         while (!feof(PipePointer))
             CompileOutput.push_back(fgetc(PipePointer));
-        if (CompileOutput != "")
+        while (CompileOutput.size() != 0 && (CompileOutput[CompileOutput.size() - 1] == '\r' || CompileOutput[CompileOutput.size() - 1] == '\n' || CompileOutput[CompileOutput.size() - 1] == 0 || CompileOutput[CompileOutput.size() - 1] == -1))
+            CompileOutput.erase(CompileOutput.size() - 1, 1);
+        if (!IsFileExists(DestFileName))
         {
-            SumaryList.push_back("#### File " + SourceFileName + " compile failed!\n" + CompileOutput + "\n\n");
+            SumaryList.push_back("### File " + SourceFileName + " compile failed!");
+            SumaryList.push_back(CompileOutput);
+            SumaryList.push_back("");
+            cout << "\"" << CompileOutput << "\"" << endl;
             CompileFailList.insert(SourceFileName);
         }
         else
@@ -54,12 +70,11 @@ set<string> GetFiles(string Path)
         else if (DirentPointer->d_type == DT_DIR)
         {
             string FolderName = DirentPointer->d_name;
-            if (FolderName == "." || FolderName == ".." || FolderName == "lib" || FolderName == "unfinish")
+            if (FolderName != "." && FolderName != ".." && FolderName != "lib" && FolderName != "unfinish" && FolderName[0] != '.')
             {
-                continue;
+                set<string> Temp = GetFiles(Path + "/" + DirentPointer->d_name);
+                FileList.insert(Temp.begin(), Temp.end());
             }
-            set<string> Temp = GetFiles(Path + "/" + DirentPointer->d_name);
-            FileList.insert(Temp.begin(), Temp.end());
         }
     }
     closedir(DirPointer);
@@ -67,10 +82,10 @@ set<string> GetFiles(string Path)
 }
 void OutputSumary(string Data)
 {
-    // StringReplaceAll(Data, "\n", "\\n");
-    // StringReplaceAll(Data, "\t", "\\t");
-    // StringReplaceAll(Data, "\r", "\\r");
-    cout << Data << endl;
+    StringReplaceAll(Data, "\n", "\\n");
+    StringReplaceAll(Data, "\t", "\\t");
+    StringReplaceAll(Data, "\r", "\\r");
+    system(string("echo \"" + Data + "\" >> $GITHUB_STEP_SUMMARY").c_str());
 }
 int main()
 {
@@ -79,19 +94,18 @@ int main()
     for (set<string>::iterator sit = FileList.begin(); sit != FileList.end(); sit++)
     {
         int PointPos;
-        if ((PointPos = sit->find_last_of(".")) == sit->npos)
+        if ((PointPos = sit->find_last_of(".")) == sit->npos || PointPos == 0)
         {
             remove(sit->c_str());
         }
         else
         {
-            string AfterPoint = sit->substr(PointPos + 1);
+            string AfterPoint = sit->substr(PointPos + 1, sit->npos);
             if (AfterPoint == "cpp" && *sit != "./DoAutoCompileWorks.cpp")
             {
                 CompileList.insert(*sit);
                 ThreadCompileList[Counter % ThreadCount].insert(*sit);
                 Counter++;
-                cout << *sit << endl;
             }
             else if ((AfterPoint == "in" || AfterPoint == "out") && sit->find("Etiger") != sit->npos)
             {
@@ -103,10 +117,15 @@ int main()
     {
         ThreadList[i] = thread(Compile, i);
     }
-    cout << endl;
     while (ThreadFinish != ThreadCount)
     {
-        cout << "\r" << CompiledList.size() << "/" << CompileList.size();
+        cout << "::group::Status Report" << endl;
+        cout << "Compile Size: " << CompileList.size() << endl;
+        cout << "Compiled Size: " << CompiledList.size() << endl;
+        cout << "Compile Success Size: " << CompileSuccessList.size() << endl;
+        cout << "Compile Fail Size: " << CompileFailList.size() << endl;
+        cout << "::endgroup::" << endl;
+        usleep(1000000);
     }
     for (int i = 0; i < ThreadCount; i++)
     {
@@ -114,16 +133,21 @@ int main()
     }
     OutputSumary("# Work Sumary");
     OutputSumary("");
-    OutputSumary("# Size");
+
+    OutputSumary("## Size");
     OutputSumary("");
     OutputSumary("- Compile Size: " + to_string(CompileList.size()));
     OutputSumary("- Compiled Size: " + to_string(CompiledList.size()));
     OutputSumary("- Compile Success Size: " + to_string(CompileSuccessList.size()));
     OutputSumary("- Compile Failed Size: " + to_string(CompileFailList.size()));
     OutputSumary("");
-    OutputSumary("# Errors");
+
+    OutputSumary("## Errors");
+    for (vector<string>::iterator vit = SumaryList.begin(); vit != SumaryList.end(); vit++)
+        OutputSumary(*vit);
     OutputSumary("");
-    OutputSumary("# File List");
+
+    OutputSumary("## File List");
     OutputSumary("");
     OutputSumary("### Compile File List");
     for (set<string>::iterator sit = CompileList.begin(); sit != CompileList.end(); sit++)
@@ -144,5 +168,6 @@ int main()
     for (set<string>::iterator sit = CompileFailList.begin(); sit != CompileFailList.end(); sit++)
         OutputSumary("- " + *sit);
     OutputSumary("");
+
     return 0;
 }
