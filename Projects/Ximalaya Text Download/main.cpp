74313c30358550aed0b8df60a7ac33a96fdb1fab
ChenLangNing <langningc2009@163.com>
2022-10-20 18:49:59 +0800

    move lib

--- Projects/Ximalaya Text Download/main.cpp
+++ Projects/Ximalaya Text Download/main.cpp
@@ -1,8 +1,8 @@
 #include "../../lib/Curl.cpp"
 #include "../../lib/lodepng.cpp"
 #include "../../lib/jpeg/jpeglib.h"
-#include "SHA1.cpp"
-#include "MD5.cpp"
+#include "../../lib/SHA1.cpp"
+#include "../../lib/MD5.cpp"
 #include <locale>
 const string WEBSign = "WEB-V1-PRODUCT-E7768904917C4154A925FBE1A3848BC3E84E2C7770744E56AFBC9600C267891F";
 const string LinuxFilePath = "/mnt/e/Ming/易经/易经(一)文字（单集到107）/";
