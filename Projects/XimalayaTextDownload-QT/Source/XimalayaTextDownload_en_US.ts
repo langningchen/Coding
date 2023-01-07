9ecc619c0fd7f6e10cb0b036fd29be18c3921990
Langning Chen <langningc2009@163.com>
2023-01-07 13:13:32 +0800

    gitignore

--- Projects/XimalayaTextDownload-QT/Source/XimalayaTextDownload_en_US.ts
+++ Projects/XimalayaTextDownload-QT/Source/XimalayaTextDownload_en_US.ts
@@ -9,12 +9,12 @@
         <translation>Ximalaya Text Download</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="41"/>
+        <location filename="mainwindow.ui" line="59"/>
         <source>Download</source>
         <translation>Download</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="98"/>
+        <location filename="mainwindow.ui" line="38"/>
         <source>Track ID: </source>
         <translation>Track ID: </translation>
     </message>
@@ -24,19 +24,19 @@
     <message>
         <location filename="mainwindow.cpp" line="35"/>
         <location filename="mainwindow.cpp" line="69"/>
-        <location filename="mainwindow.cpp" line="94"/>
-        <location filename="mainwindow.cpp" line="190"/>
-        <location filename="mainwindow.cpp" line="199"/>
-        <location filename="mainwindow.cpp" line="261"/>
-        <location filename="mainwindow.cpp" line="284"/>
-        <location filename="mainwindow.cpp" line="303"/>
-        <location filename="mainwindow.cpp" line="321"/>
-        <location filename="mainwindow.cpp" line="332"/>
-        <location filename="mainwindow.cpp" line="341"/>
-        <location filename="mainwindow.cpp" line="351"/>
-        <location filename="mainwindow.cpp" line="363"/>
-        <location filename="mainwindow.cpp" line="393"/>
-        <location filename="mainwindow.cpp" line="410"/>
+        <location filename="mainwindow.cpp" line="96"/>
+        <location filename="mainwindow.cpp" line="197"/>
+        <location filename="mainwindow.cpp" line="212"/>
+        <location filename="mainwindow.cpp" line="288"/>
+        <location filename="mainwindow.cpp" line="312"/>
+        <location filename="mainwindow.cpp" line="337"/>
+        <location filename="mainwindow.cpp" line="361"/>
+        <location filename="mainwindow.cpp" line="377"/>
+        <location filename="mainwindow.cpp" line="387"/>
+        <location filename="mainwindow.cpp" line="398"/>
+        <location filename="mainwindow.cpp" line="411"/>
+        <location filename="mainwindow.cpp" line="443"/>
+        <location filename="mainwindow.cpp" line="461"/>
         <source>错误</source>
         <translation>Error</translation>
     </message>
@@ -47,79 +47,79 @@
         <translation>Can not open file </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="94"/>
+        <location filename="mainwindow.cpp" line="96"/>
         <source>获得随机数值失败 </source>
         <translation>Get number once failed </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="190"/>
+        <location filename="mainwindow.cpp" line="197"/>
         <source>多次失败，请重新运行程序重试</source>
         <translation>Failed for many times. Please rerun the program to retry</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="199"/>
+        <location filename="mainwindow.cpp" line="212"/>
         <source>获得验证码失败 </source>
         <translation>Get captcha failed </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="261"/>
-        <location filename="mainwindow.cpp" line="303"/>
+        <location filename="mainwindow.cpp" line="288"/>
+        <location filename="mainwindow.cpp" line="337"/>
         <source>验证码校验失败 </source>
         <translation>Check captcha failed </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="263"/>
+        <location filename="mainwindow.cpp" line="288"/>
         <source>匹配到的滑块位置为 </source>
         <translation>The matching slider position is </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="265"/>
+        <location filename="mainwindow.cpp" line="288"/>
         <source>正在重试</source>
         <translation>. Retrying</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="284"/>
+        <location filename="mainwindow.cpp" line="312"/>
         <source>验证码发送失败 </source>
         <translation>SMS message send failed </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="293"/>
+        <location filename="mainwindow.cpp" line="323"/>
         <source>请输入验证码</source>
         <translation>Please input the SMS verification code</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="321"/>
+        <location filename="mainwindow.cpp" line="361"/>
         <source>登录失败 </source>
         <translation>Login failed </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="332"/>
-        <location filename="mainwindow.cpp" line="351"/>
+        <location filename="mainwindow.cpp" line="377"/>
+        <location filename="mainwindow.cpp" line="398"/>
         <source>获取合集信息失败 </source>
         <translation>Get album information failed </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="341"/>
+        <location filename="mainwindow.cpp" line="387"/>
         <source>请输入大于1小于</source>
         <translation>Please input number that is greater than 1 and less than </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="341"/>
+        <location filename="mainwindow.cpp" line="387"/>
         <source>的数字</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="363"/>
+        <location filename="mainwindow.cpp" line="411"/>
         <source>获取音频信息失败 </source>
         <translation>Get track information failed </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="393"/>
+        <location filename="mainwindow.cpp" line="443"/>
         <source>打开输出文件失败</source>
         <translation>Open output file failed</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="410"/>
+        <location filename="mainwindow.cpp" line="461"/>
         <source>执行失败</source>
         <translation>Execute failed</translation>
     </message>
--- Projects/XimalayaTextDownload-QT/Source/XimalayaTextDownload_zh_CN.ts
+++ Projects/XimalayaTextDownload-QT/Source/XimalayaTextDownload_zh_CN.ts
@@ -9,12 +9,12 @@
         <translation>喜马拉雅文字下载</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="41"/>
+        <location filename="mainwindow.ui" line="59"/>
         <source>Download</source>
         <translation>下载</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="98"/>
+        <location filename="mainwindow.ui" line="38"/>
         <source>Track ID: </source>
         <translation>音频编号： </translation>
     </message>
@@ -24,19 +24,19 @@
     <message>
         <location filename="mainwindow.cpp" line="35"/>
         <location filename="mainwindow.cpp" line="69"/>
-        <location filename="mainwindow.cpp" line="94"/>
-        <location filename="mainwindow.cpp" line="190"/>
-        <location filename="mainwindow.cpp" line="199"/>
-        <location filename="mainwindow.cpp" line="261"/>
-        <location filename="mainwindow.cpp" line="284"/>
-        <location filename="mainwindow.cpp" line="303"/>
-        <location filename="mainwindow.cpp" line="321"/>
-        <location filename="mainwindow.cpp" line="332"/>
-        <location filename="mainwindow.cpp" line="341"/>
-        <location filename="mainwindow.cpp" line="351"/>
-        <location filename="mainwindow.cpp" line="363"/>
-        <location filename="mainwindow.cpp" line="393"/>
-        <location filename="mainwindow.cpp" line="410"/>
+        <location filename="mainwindow.cpp" line="96"/>
+        <location filename="mainwindow.cpp" line="197"/>
+        <location filename="mainwindow.cpp" line="212"/>
+        <location filename="mainwindow.cpp" line="288"/>
+        <location filename="mainwindow.cpp" line="312"/>
+        <location filename="mainwindow.cpp" line="337"/>
+        <location filename="mainwindow.cpp" line="361"/>
+        <location filename="mainwindow.cpp" line="377"/>
+        <location filename="mainwindow.cpp" line="387"/>
+        <location filename="mainwindow.cpp" line="398"/>
+        <location filename="mainwindow.cpp" line="411"/>
+        <location filename="mainwindow.cpp" line="443"/>
+        <location filename="mainwindow.cpp" line="461"/>
         <source>错误</source>
         <translation>错误</translation>
     </message>
@@ -47,79 +47,79 @@
         <translation>无法打开文件</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="94"/>
+        <location filename="mainwindow.cpp" line="96"/>
         <source>获得随机数值失败 </source>
         <translation>获得随机数值失败 </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="190"/>
+        <location filename="mainwindow.cpp" line="197"/>
         <source>多次失败，请重新运行程序重试</source>
         <translation>多次失败，请重新运行程序重试</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="199"/>
+        <location filename="mainwindow.cpp" line="212"/>
         <source>获得验证码失败 </source>
         <translation>获得验证码失败 </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="261"/>
-        <location filename="mainwindow.cpp" line="303"/>
+        <location filename="mainwindow.cpp" line="288"/>
+        <location filename="mainwindow.cpp" line="337"/>
         <source>验证码校验失败 </source>
         <translation>验证码校验失败 </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="263"/>
+        <location filename="mainwindow.cpp" line="288"/>
         <source>匹配到的滑块位置为 </source>
         <translation>匹配到的滑块位置为 </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="265"/>
+        <location filename="mainwindow.cpp" line="288"/>
         <source>正在重试</source>
         <translation>正在重试</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="284"/>
+        <location filename="mainwindow.cpp" line="312"/>
         <source>验证码发送失败 </source>
         <translation>验证码发送失败 </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="293"/>
+        <location filename="mainwindow.cpp" line="323"/>
         <source>请输入验证码</source>
         <translation>请输入验证码</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="321"/>
+        <location filename="mainwindow.cpp" line="361"/>
         <source>登录失败 </source>
         <translation>登录失败 </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="332"/>
-        <location filename="mainwindow.cpp" line="351"/>
+        <location filename="mainwindow.cpp" line="377"/>
+        <location filename="mainwindow.cpp" line="398"/>
         <source>获取合集信息失败 </source>
         <translation>获取合集信息失败 </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="341"/>
+        <location filename="mainwindow.cpp" line="387"/>
         <source>请输入大于1小于</source>
         <translation>请输入大于1小于</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="341"/>
+        <location filename="mainwindow.cpp" line="387"/>
         <source>的数字</source>
         <translation>的数字</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="363"/>
+        <location filename="mainwindow.cpp" line="411"/>
         <source>获取音频信息失败 </source>
         <translation>获取音频信息失败 </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="393"/>
+        <location filename="mainwindow.cpp" line="443"/>
         <source>打开输出文件失败</source>
         <translation>打开输出文件失败</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="410"/>
+        <location filename="mainwindow.cpp" line="461"/>
         <source>执行失败</source>
         <translation>执行失败</translation>
     </message>
