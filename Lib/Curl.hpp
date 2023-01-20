a5d4561ebb16d247cdda88dab818ddb39553dc9f
Langning Chen <langningc2009@163.com>
2023-01-20 03:17:21 -0500

    beautifier curl and delete a useless function

--- Lib/Curl.hpp
+++ Lib/Curl.hpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <curl/curl.h>
 #include <unistd.h>
+#include <sys/time.h>
 #include "./configor/json.hpp"
 #include "./StringOperation.hpp"
 using namespace std;
@@ -11,203 +12,6 @@ const string WECHAT_UA = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36
 const string FORM = "application/x-www-form-urlencoded";
 const string MULTIPART_BOUNDARY = "qv5wyfw459yhugv5swbmq39m8yuw4";
 const string MULTIPART = "multipart/form-data; boundary=" + MULTIPART_BOUNDARY;
-string HTTPResponseString(int HTTPResponseCode)
-{
-    switch (HTTPResponseCode)
-    {
-    case 100:
-        return "Continue";
-        break;
-    case 101:
-        return "Switching Protocols";
-        break;
-    case 102:
-        return "Processing";
-        break;
-    case 103:
-        return "Early Hints";
-        break;
-    case 200:
-        return "OK";
-        break;
-    case 201:
-        return "Created";
-        break;
-    case 202:
-        return "Accepted";
-        break;
-    case 203:
-        return "Non-Authoritative Information";
-        break;
-    case 204:
-        return "No Content";
-        break;
-    case 205:
-        return "Reset Content";
-        break;
-    case 206:
-        return "Partial Content";
-        break;
-    case 207:
-        return "Multi-Status";
-        break;
-    case 208:
-        return "Already Reported";
-        break;
-    case 226:
-        return "IM Used";
-        break;
-    case 300:
-        return "Multiple Choice";
-        break;
-    case 301:
-        return "Moved Permanently";
-        break;
-    case 302:
-        return "Found";
-        break;
-    case 303:
-        return "See Other";
-        break;
-    case 304:
-        return "Not Modified";
-        break;
-    case 305:
-        return "Use Proxy";
-        break;
-    case 306:
-        return "unused";
-        break;
-    case 307:
-        return "Temporary Redirect";
-        break;
-    case 308:
-        return "Permanent Redirect";
-        break;
-    case 400:
-        return "Bad Request";
-        break;
-    case 401:
-        return "Unauthorized";
-        break;
-    case 402:
-        return "Payment Required";
-        break;
-    case 403:
-        return "Forbidden";
-        break;
-    case 404:
-        return "Not Found";
-        break;
-    case 405:
-        return "Method Not Allowed";
-        break;
-    case 406:
-        return "Not Acceptable";
-        break;
-    case 407:
-        return "Proxy Authentication Required";
-        break;
-    case 408:
-        return "Request Timeout";
-        break;
-    case 409:
-        return "Conflict";
-        break;
-    case 410:
-        return "Gone";
-        break;
-    case 411:
-        return "Length Required";
-        break;
-    case 412:
-        return "Precondition Failed";
-        break;
-    case 413:
-        return "Payload Too Large";
-        break;
-    case 414:
-        return "URI Too Long";
-        break;
-    case 415:
-        return "Unsupported Media Type";
-        break;
-    case 416:
-        return "Range Not Satisfiable";
-        break;
-    case 417:
-        return "Expectation Failed";
-        break;
-    case 418:
-        return "I'm a teapot";
-        break;
-    case 421:
-        return "Misdirected Request";
-        break;
-    case 422:
-        return "Unprocessable Entity";
-        break;
-    case 423:
-        return "Locked";
-        break;
-    case 424:
-        return "Failed Dependency";
-        break;
-    case 425:
-        return "Too Early";
-        break;
-    case 426:
-        return "Upgrade Required";
-        break;
-    case 428:
-        return "Precondition Required";
-        break;
-    case 429:
-        return "Too Many Requests";
-        break;
-    case 431:
-        return "Request Header Fields Too Large";
-        break;
-    case 451:
-        return "Unavailable For Legal Reasons";
-        break;
-    case 500:
-        return "Internal Server Error";
-        break;
-    case 501:
-        return "Not Implemented";
-        break;
-    case 502:
-        return "Bad Gateway";
-        break;
-    case 503:
-        return "Service Unavailable";
-        break;
-    case 504:
-        return "Gateway Timeout";
-        break;
-    case 505:
-        return "HTTP Version Not Supported";
-        break;
-    case 506:
-        return "Variant Also Negotiates";
-        break;
-    case 507:
-        return "Insufficient Storage";
-        break;
-    case 508:
-        return "Loop Detected";
-        break;
-    case 510:
-        return "Not Extended";
-        break;
-    case 511:
-        return "Network Authentication Required";
-        break;
-    default:
-        return to_string(HTTPResponseCode);
-    }
-}
 struct PROGRESS
 {
     CURL *Curl;
@@ -217,22 +21,24 @@ int GetDataToFileProgressCallback(void *_Param,
                                   curl_off_t UploadTotal, curl_off_t UploadNow)
 
 {
+    if (DownloadTotal == 0)
+        return 0;
     struct PROGRESS *Param = (struct PROGRESS *)_Param;
     CURL *Curl = Param->Curl;
-    string OutputString = "";
     double Speed = 0;
     curl_easy_getinfo(Curl, CURLINFO_SPEED_DOWNLOAD, &Speed);
 
     // Output download progress bar
-    string Bar = "[";
-    for (int i = 0; i < 50; i++)
-    {
-        if (i < DownloadNow * 1.0 / DownloadTotal * 50)
-            Bar += "█";
-        else
-            Bar += " ";
-    }
-    Bar += "]";
+    string ProgressBarString[8] = {"▏", "▎", "▍", "▌", "▋", "▊", "▉", "█"};
+    string CurrentProgressBarString = "[";
+    int ProgressBarLength = 20;
+    double CurrentProgressBarLength = DownloadNow * 1.0 / DownloadTotal * ProgressBarLength;
+    for (int i = 0; i < floor(CurrentProgressBarLength); i++)
+        CurrentProgressBarString += ProgressBarString[7];
+    CurrentProgressBarString += ProgressBarString[(int)((CurrentProgressBarLength - floor(CurrentProgressBarLength)) * 8)];
+    for (int i = 0; i < ProgressBarLength - ceil(CurrentProgressBarLength); i++)
+        CurrentProgressBarString += " ";
+    CurrentProgressBarString += "]";
 
     // Output network status
     string NetworkStatus;
@@ -278,7 +84,6 @@ int GetDataToFileProgressCallback(void *_Param,
     int HoursLeft = -1;
     int MinutesLeft = -1;
     int SecondsLeft = -1;
-    OutputString += " Remaining: ";
     if (Speed != 0)
     {
         double TimeLeft = (DownloadTotal - DownloadNow) / Speed;
@@ -287,11 +92,17 @@ int GetDataToFileProgressCallback(void *_Param,
         SecondsLeft = TimeLeft - HoursLeft * 3600 - MinutesLeft * 60;
     }
 
+    string LoadingString[8] = {"⣰", "⣤", "⣆", "⡇", "⠏", "⠛", "⠹", "⢸"};
+    struct timeval tv;
+    struct timezone tz;
+    gettimeofday(&tv, &tz);
+    string CurrentLoadingString = LoadingString[(int)(tv.tv_usec / 125000) % 8];
+
     // Output and flush
-    printf("\033[2K\r%.2f%% %s %s %s %.2f%s %02d:%02d:%02d",
+    printf("\033[2K\r%.2f%% %s%s %s %.2f%s %02d:%02d:%02d",
            DownloadNow * 1.0 / DownloadTotal * 100,
-           OutputString.c_str(),
-           Bar.c_str(),
+           CurrentLoadingString.c_str(),
+           CurrentProgressBarString.c_str(),
            NetworkStatus.c_str(),
            SpeedInUnit,
            Unit.c_str(),
